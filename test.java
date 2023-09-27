0613053f5a178d3d6e325f5c51a99bacc7d8ebcf
AlexMarchese <alex99marchese@gmail.com>
2023-09-27 08:41:11 +0200

    test

---
+++ test.java
@@ -0,0 +1,17 @@
+public class Test
+{
+    // instance variables - replace the example below with your own
+    private int limit;
+    private int wert;
+    
+
+
+    /**
+     * Constructor for objects of class Nummernanzeige
+     */
+    public Nummernanzeige(int anzeigeLimit)
+    {
+        // initialise instance variables
+        limit = anzeigeLimit;
+        wert = 0;
+    }
