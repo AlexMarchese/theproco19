b82efffd1f18d1503a26d3e728bf168d0662819b
AlexMarchese <alex99marchese@gmail.com>
2023-09-27 08:52:22 +0200

    Bluej

---
+++ Fabrik.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Fabrik here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fabrik
+{
+   
+
+}
